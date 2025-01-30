60cf76bf38bc3fcb553a8c4c45bfe78a021069de
boerdie92 <jonathan.doering92@gmail.com>
2025-01-30 23:38:18 +0100

    final

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    #word_counter(file_contents)
-    result = letter_counter(file_contents)
-    print(result)
+filename = "books/frankenstein.txt"
 
-#commented out since not needed for task!
+def main():
+    with open(filename) as f:
+        file_contents = f.read()
+    words_counted = word_counter(file_contents)
+    letters_returned = letter_counter(file_contents)
+    sorted_chars = count_sorter(letters_returned)
+    print_results(words_counted, sorted_chars)
+
 def word_counter(whole_string):
-    count = 0
-    words = whole_string.split()
-    for i in range(0,len(words)):
-        count += 1
-    print(count)
+    return len(whole_string.split())
 
 def letter_counter(whole_string):
     letters = {}
@@ -22,5 +20,24 @@ def letter_counter(whole_string):
         else:
             letters[letter]= 1
     return letters
+
+def count_sorter(counted_chars):
+    alphabet = []
+    for entry in counted_chars:
+        if entry.isalpha() == True:
+            whole = {}
+            whole["chars"] = entry
+            whole["count"] = counted_chars[entry]
+            alphabet.append(whole)
+    alphabet.sort(reverse=True, key=lambda x: x["count"])
+    return alphabet
+
+def print_results(n_words, chars_):
+    print(f"--- Begin report of {filename} ---")
+    print(f"{n_words} words found in the document")
+    print("")
+    for entry in chars_:
+        print(f"The '{entry["chars"]}' character was found {entry["count"]} times")
+    print("--- End report ---")
 
 main()
